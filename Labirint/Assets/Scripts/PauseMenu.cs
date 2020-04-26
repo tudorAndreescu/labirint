b28625a6a3a618b227653c022203c1f1593ccde0
Tudor Andreescu <tudor.andreescu.dev@gmail.com>
2020-04-26 15:55:03 +0300

    Implemented Pause Menu
    Working by pressing "esc"; resume button and quit button working, still need to implement the menu scene

---
+++ Labirint/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) Resume();
+            else Pause();
+        }
+    }
+
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+
+        ///Instead of printing, load menu scene here once it is up and running
+        ///use a variable instead of hard coding
+        //SceneManager.LoadScene("Menu");
+        //Time.timeScale = 1f;
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+
+
+}
