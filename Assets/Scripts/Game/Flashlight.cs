d17b41d733d1ca6d43d98553dcfd075f0561781d
Tudor Andreescu <tudor.andreescu.dev@gmail.com>
2020-05-03 20:33:16 +0300

    Implemented Batteries Display System

--- Assets/Scripts/Game/Flashlight.cs
+++ Assets/Scripts/Game/Flashlight.cs
@@ -1,28 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Flashlight : MonoBehaviour
 {
-    private bool flashlightEnabled;
-    public GameObject flashlight;
+    public GameObject[] batteries;
     public GameObject spotlight;
-    public float maxEnergy;
-    public static float currentEnergy;
+    public TextMeshProUGUI energyTimerText;
+    public GameObject energyTimer;
 
-    private GameObject batteryPickedUp;
-    private float usedEnergy;
+    private bool flashlightEnabled;
+    private float maxEnergy;
+    private int numberOfBatteriesToDisplay = 0;
+    private int maxNumberOfBatteries = 0;
+
+    public static float currentEnergy;
 
 
    public void Start()
     {
+        //for(int i = 0; i < batteries.Length; i++) batteries[i].enabled = false;
         maxEnergy = 60 * StaticValues.GetNumberOfBatteries(); 
         currentEnergy = 60;
+        maxNumberOfBatteries = StaticValues.GetNumberOfBatteries();
+        if (maxNumberOfBatteries == 0) maxNumberOfBatteries = 1;
+        UpdateBatteries();
     }
 
  
     public void Update()
     {
+        numberOfBatteriesToDisplay = (int)currentEnergy/60 + 1;
+        UpdateBatteries();
+
         //equip
         if (Input.GetKeyDown(KeyCode.F))
             flashlightEnabled =! flashlightEnabled;
@@ -47,8 +59,20 @@ public class Flashlight : MonoBehaviour
            spotlight.SetActive(false);
         }
 
+        if (currentEnergy <= 15) {
+            energyTimer.SetActive(true);
+            energyTimerText.SetText($"{(int)currentEnergy}");
+        } else energyTimer.SetActive(false);
+
         
     }
 
-    
+
+    private void UpdateBatteries() {
+        for(int i = 0; i < maxNumberOfBatteries; i++) {
+            if (i < numberOfBatteriesToDisplay) batteries[i].SetActive(true);
+            else batteries[i].SetActive(false);
+        }
+    } 
+
 }
